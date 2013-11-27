fe5e1dc6b7f12d533d163845fdfd1b389c173914
Daniele Murroni <empirico@gmail.com>
2013-11-27 01:24:48 +0100

    preload js

---
+++ site/js/app.js
@@ -0,0 +1,56 @@
+var assets = [
+
+    "img/bg.jpg",
+    "img/moon.png",
+    "img/old",
+    "img/stars_back.png",
+    "img/stars_front.png",
+    "img/stars_middle.png",
+    "img/wave_far.png",
+    "img/wave_front.png",
+    "img/wave_medium-back.png",
+    "img/wave_medium.png",
+
+    "js/jquery.js",
+    "js/parallax.js",
+    "js/jquery.parallax.js",
+    "js/main.js",
+
+    "css/style.css"
+
+];
+
+var scene = {};
+var onQueueProgress = function () {
+    document.getElementById('counter').innerHTML = Math.floor(queue.progress * 100) + '%';
+}
+
+var onQueueLoaded = function () {
+    activateStage();
+}
+
+var handleFileLoad = function (e) {
+    if (e.item.type === 'javascript') {
+        //console.log(e.item.src);
+        document.getElementsByTagName('body')[0].appendChild(e.result);
+    }
+    else if (e.item.type === 'css') {
+        document.getElementsByTagName('head')[0].appendChild(e.result);
+    }
+}
+
+
+var queue = new createjs.LoadQueue(true);
+
+queue.setMaxConnections(1);
+queue.addEventListener("fileload", handleFileLoad);
+queue.addEventListener("complete", onQueueLoaded);
+queue.addEventListener("progress", onQueueProgress);
+
+for (var n = 0; n < assets.length; n++) {
+    queue.loadFile("/" + assets[n], false);
+}
+
+queue.load();
+
+
--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,14 @@
-$("document").ready(function(){
-   // $(".container").height($(window).height());
+function activateStage() {
+    scene = $('#scene').parallax();
+    $("#counter").fadeOut("slow", function(){
+        $("#container").fadeIn();
+    });
+}
 
-});
+$(window).resize(function(){
+
+    $('#scene').height(window.innerHeight);
+    $('#scene').width(window.innerWidth*2);
+    $("#container").height(window.innerHeight);
+    $("#container").width(window.innerWidth);
+})
